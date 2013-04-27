3b4a55ed7182933dcb1d71d63b9a5cfe4b42d9c9
Zenossnake <the_je4@yahoo.com>
2013-04-27 15:33:40 -0500

    Update spimbot.s

--- spimbot.s
+++ spimbot.s
@@ -5,6 +5,21 @@
 ###     Spimbot contest team name: 
 ###
 ###
+
+
+# spimbot constants
+ANGLE = 0xffff0014
+ANGLE_CONTROL = 0xffff0018
+TIMER = 0xffff001c
+HEAD_X = 0xffff0020
+HEAD_Y = 0xffff0024
+BONK_ACKNOWLEDGE = 0xffff0060
+TIMER_ACKNOWLEDGE = 0xffff006c
+PUBLIC_APPLE_X = 0xffff0070
+PUBLIC_APPLE_Y = 0xffff0074
+PRIVATE_APPLE_X = 0xffff00b0
+PRIVATE_APPLE_Y = 0xffff00b4
+
 .kdata                # interrupt handler data (separated just for readability)
 chunkIH:.space 52      # space for the registers
 non_intrpt_str:   .asciiz "Non-interrupt exception\n"
@@ -49,6 +64,10 @@ interrupt_dispatch:                    # Interrupt:
       and     $a0, $k0, 0x4000		#is there a new apple interrupt?
       bne     $a0, 0, new_public_apple_interrupt
       
+      and	$a0, $k0, 0x2000	#is there a new private apple created?
+      bne	$a0, $0, new_private_apple_interrupt
+    
+
 
       
       #puzzle interrupt
@@ -63,6 +82,14 @@ interrupt_dispatch:                    # Interrupt:
       syscall 
       j       done
 
+
+
+
+
+
+	#bonk interrupt which makes the snake stop until
+	#the obstacle no longer is in the way
+	#TO BE MODIFIED ONCE COLLISION IS IMPLEMENTED
 bonk_interrupt:
       sw $zero, 0xffff0010($zero) # Forces snake to stop
       sw $a1, 0xffff0060($zero) # acknowledge interrupt
@@ -71,7 +98,17 @@ bonk_interrupt:
 
 
 
-new_apple_interrupt:
+
+
+
+
+
+
+	#interrupt that will be called	
+	#when the public apple was eaten
+	#or a new public apple has appeared
+	#same thing really
+new_public_apple_interrupt:
       sw      $a1, 0xffff0064($zero)   # acknowledge interrupt
       #need to save the used registers on the stack
       
@@ -79,8 +116,8 @@ new_apple_interrupt:
 	lw $t0, HEAD_X
 
 	lw $t1, HEAD_Y
-	lw $a2, APPLE_X
-	lw $a3, APPLE_Y
+	lw $a2, PUBLIC_APPLE_X
+	lw $a3, PUBLIC_APPLE_Y
 	
 	
 	beq $t0, $a2, first_if_public_apple # my_x != apple_x
@@ -136,15 +173,96 @@ public_apple_set_abs_angle:
 	sw $t2, prev_angle
 	
       j interrupt_dispatch # see if other interrupts are waiting      
+
+
+	
+	
+	
+	
+	#interrupt that occurs when
+	#a new private apple is available
+	#UNDER CONSTRUCTION
+	#SEE HOW MATRIX PERFORMS
+new_private_apple_interrupt:
+      sw      $a1, 0xffff0064($zero)   # acknowledge interrupt
+      #need to save the used registers on the stack
       
       
+	lw $t0, HEAD_X
+
+	lw $t1, HEAD_Y
+	lw $a2, PRIVATE_APPLE_X
+	lw $a3, PRIVATE_APPLE_Y
+	
+	
+	beq $t0, $a2, first_if_private_apple # my_x != apple_x
+	bge $t0, $a2, second_if_private_apple # my_x < apple_x
+	add $t2, $0, 0
+	j private_apple_end # returns 0
+	
+second_if_private_apple: # second if not true
+	add $t2, $0, 180 # inputs 180 for desired angle
+	j private_apple_end # returns 180
+	
+first_if_private_apple:
+	bge $t1, $a3, third_if_private_apple # my_y < apple_y
+	add $t2, $0, 90 # inputs 90 for v0
+	j private_apple_end # returns 90
+	
+third_if_private_apple:
+	add $t2, $0, 270 # inputs 270 into v0
+	j private_apple_end # returns 270
+	
+private_apple_end:
+	
+	lw $t0, prev_angle
+	sub $t0, $t0, $t2
+	li $a2, 180
+	
+	bge $t0, $0, private_apple_less_than_zero
+	sub $t0, $0, $t0
+	
+private_apple_less_than_zero:
+	
+	bne $t0, $a2, private_apple_not_equal_180
+	
+	add $t2, $t2, 90
+	
+	li $t0, 0
+	li $a2, 5000
+	
+private_apple_for_loop:
+	add $t0, $t0, 1
+	bne $t0, $a2, private_apple_for_loop
+	
+private_apple_not_equal_180:
+	
+	bge $t2, $0, private_apple_set_abs_angle # sets abs angle
+	add $t2, $0, $t2
+	
+private_apple_set_abs_angle:
+	
+        sw $t2, 0xffff0014($zero) # Sets angle to proper angle
+	li $t0, 1
+        sw $t0, 0xffff0018($zero) # relative to zero
+	sw $t2, prev_angle
+	
+      j interrupt_dispatch # see if other interrupts are waiting          
+      
+	
+	
+	
+	
+		
+      
+	#UNDER CONSTRUCTION      
 timer_interrupt:
       sw $a1, 0xffff006c($zero) # acknowledge interrupt
  
 	lw $t0, HEAD_X
 	lw $t1, HEAD_Y
-	lw $a2, APPLE_X
-	lw $a3, APPLE_Y
+	lw $a2, PUBLIC_APPLE_X
+	lw $a3, PUBLIC_APPLE_Y
 
 
 	beq $t0, $a2, first_if # my_x != apple_x
